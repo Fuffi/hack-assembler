c3cbcdf48681d8995a523c8fe7890e180e6a521c
Federico Rebora <ufinii@gmail.com>
2015-08-23 22:37:29 +0100

    Computation Translator fourth pass

--- test/computation_translator_test.rb
+++ test/computation_translator_test.rb
@@ -37,5 +37,39 @@ class ComputationTranslatorTest < Minitest::Test
     assert_equal '1110001', ComputationTranslator.translate('!M')
   end
 
+  def test_that_it_converts_minus_d_register
+    assert_equal '0001111', ComputationTranslator.translate('-D')
+  end
 
+  def test_that_it_converts_minus_a_register
+    assert_equal '0110011', ComputationTranslator.translate('-A')
+  end
+
+  def test_that_it_converts_minus_m_register
+    assert_equal '1110011', ComputationTranslator.translate('-M')
+  end
+
+  def test_that_it_converts_d_register_plus_one
+    assert_equal '0011111', ComputationTranslator.translate('D+1')
+  end
+
+  def test_that_it_converts_a_register_plus_one
+    assert_equal '0110111', ComputationTranslator.translate('A+1')
+  end
+
+  def test_that_it_converts_m_register_plus_one
+    assert_equal '1110111', ComputationTranslator.translate('M+1')
+  end
+
+  def test_that_it_converts_d_register_minus_one
+    assert_equal '0001110', ComputationTranslator.translate('D-1')
+  end
+
+  def test_that_it_converts_a_register_minus_one
+    assert_equal '0110010', ComputationTranslator.translate('A-1')
+  end
+
+  def test_that_it_converts_m_register_minus_one
+    assert_equal '1110010', ComputationTranslator.translate('M-1')
+  end
 end
