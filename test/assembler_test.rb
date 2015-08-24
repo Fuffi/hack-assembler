f45fdf59f7d741be1bff1260d65ca8f30a0be2bf
Federico Rebora <ufinii@gmail.com>
2015-08-24 21:10:23 +0100

    Minor refactor for AssemblerTest

--- test/assembler_test.rb
+++ test/assembler_test.rb
@@ -4,7 +4,7 @@ class AssemblerTest < Minitest::Test
   def test_translates_a_and_c_instructions
     source_code = "@4\n" << "0;JMP\n"
     bytecode = Assembler.translate(source_code)
-    assert_equal "0000000000000100\n1110101010000111\n", bytecode
+    assert_equal "0000000000000100\n" << "1110101010000111\n", bytecode
   end
 
   def test_ignores_empty_lines
