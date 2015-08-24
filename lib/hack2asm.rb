d37ab9c63c83e5643a854cefa22efb340d619380
Federico <federico.rebora@carwow.co.uk>
2015-08-24 12:36:52 +0000

    Temporary fixes for reading existing asm files

--- lib/hack2asm.rb
+++ lib/hack2asm.rb
@@ -10,16 +10,14 @@ module Hack2asm
   def self.translate_file(input_file: 'PongL.asm', output_file: 'out.asm')
     source_file = File.open(input_file)
     source_code = source_file.read
+    source_code = source_code.gsub(/^[\s]*$\n/, '')
 
     source_code.each_line do |line|
       next if line.start_with?('//')
 
       instruction = line.start_with?('@') ? AInstruction : CInstruction
-      begin
-        puts instruction.translate(line.trim)
-      rescue ParserError
-        puts "Error parsing line: #{line}"
-      end
+
+      puts instruction.translate(line.strip)
     end
 
     source_file.close
